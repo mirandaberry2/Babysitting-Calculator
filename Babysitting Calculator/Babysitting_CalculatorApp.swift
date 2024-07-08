9aeb3e0d8347a2a112c69dd5da9d8083a26b6d48
mirandaberry2 <mjmarck2@gmail.com>
2024-07-07 22:13:01 -0600

    Initial Commit

---
+++ Babysitting Calculator/Babysitting_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Babysitting_CalculatorApp.swift
+//  Babysitting Calculator
+//
+//  Created by Miranda Marck on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Babysitting_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
